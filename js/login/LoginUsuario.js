c2d66245db815362905e64b0e509f48eba8d98f2
Kayo Silva <kayo.silva@grancursosonline.com.br>
2022-02-22 10:42:34 -0300

    Implementações para salvar os cards no localstorage para cada usuario

--- js/login/LoginUsuario.js
+++ js/login/LoginUsuario.js
@@ -1,18 +1,23 @@
-const userLogado = JSON.parse(localStorage.getItem('logado')) ?? {userName: '', logado: false};
+let userLogado = JSON.parse(localStorage.getItem('logado')) ?? {userName: '', logado: false};
 let logado = userLogado.logado;
+const User = new EventEmitter2();
 
 LoginUsuario_render({
     logado,
     usuario: userLogado.userName,
     onLogin: (userName) => {
         logado = true;
-        localStorage.setItem('logado', JSON.stringify({
+        const objUser = {
             userName,
             logado: logado,
-        }));
+        };
+        localStorage.setItem('logado', JSON.stringify(objUser));
+        userLogado = objUser;
+        User.emit('login');
     },
     onLogout: () => {
         logado = false;
         localStorage.removeItem('logado');
+        User.emit('logout');
     },
 });
--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,21 +1,63 @@
 const Mural = (function (_render, Filtro) {
     'use strict';
-    let cartoes = [];
+    let localStorageKeyname = `cartoes:${userLogado.userName}`;
+    let cartoes = pegaCartoesUsuarios();
+
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+    render();
+
+    User.on('login', () => {
+        localStorageKeyname = `cartoes:${userLogado.userName}`;
+        cartoes = pegaCartoesUsuarios();
+        render();
+    });
+
+    User.on('logout', () => {
+        // localStorage.removeItem(userLogado.userName);
+        cartoes = [];
+        render();
+    });
 
     Filtro.on('filtrado', render);
+
+    function pegaCartoesUsuarios() {
+        const cartoesLocal = JSON.parse(localStorage.getItem(localStorageKeyname));
+        if (cartoesLocal) {
+            return cartoesLocal.map(row => {
+                const cartao = new Cartao(row.conteudo, row.tipo);
+                preparaCartao(cartao);
+                return cartao;
+            });
+        }
+        return [];
+    }
+
+    function preparaCartao(cartao) {
+        cartao.on('mudanca.**', salvaCartoes);
+        cartao.on('remocao', () => {
+            cartoes = cartoes.slice(0);
+            cartoes.splice(cartoes.indexOf(cartao), 1);
+            salvaCartoes();
+            render();
+        });
+    }
+
+    function salvaCartoes() {
+        const cartoesParsed = cartoes.map(cartao => ({
+            conteudo: cartao.conteudo,
+            tipo: cartao.tipo,
+        }));
+        localStorage.setItem(localStorageKeyname, JSON.stringify(cartoesParsed));
+    }
 
     function adiciona(cartao) {
         if (!logado) {
             return false;
         }
+        console.log(cartao);
         cartoes.push(cartao);
-        cartao.on('mudanca.**', render);
-        cartao.on('remocao', () => {
-            cartoes = cartoes.slice(0);
-            cartoes.splice(cartoes.indexOf(cartao), 1);
-            render();
-        });
+        preparaCartao(cartao);
+        salvaCartoes();
         render();
         return true;
     }
